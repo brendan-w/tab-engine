839d9aa0f15a613435fa389bb0ad492427a77c1b
Brendan Whitfield <brendanw@windworksdesign.com>
2014-10-26 12:35:18 -0400

    switched to readFileSync

--- src/app.js
+++ src/app.js
@@ -2,31 +2,10 @@
 var fs = require("fs");
 var parse = require("./parse.js");
 
+tab1 = fs.readFileSync("./tests/eruption2.tab", "utf8");
+tab2 = fs.readFileSync("./tests/eruption.tab", "utf8");
 
-tab1 = "";
-tab2 = "";
+m1 = parse(tab1, {});
+m2 = parse(tab2, {});
 
-//fs.readFile("./tests/eruption2.tab", "utf8", function(err, tab) {
-fs.readFile("./tests/A.tab", "utf8", function(err, tab) {
-	if(err)
-	{
-		console.log(err);
-	}
-	else
-	{
-		tab1 = tab;
-		fs.readFile("./tests/B.tab", "utf8", function(err, tab) {
-			if(err)
-			{
-				console.log(err);
-			}
-			else
-			{
-				tab2 = tab;
-				m1 = parse(tab1, {});
-				m2 = parse(tab2, {});
-				m1.compare(m2);
-			}
-		});
-	}
-});
+m1.compare(m2);
